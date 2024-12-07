36f4a8e93bbb4d8e189f826e98a43d0b866ae9dd
Pupcikas <domasglad8@gmail.com>
2024-12-07 22:52:19 +0200

    a

--- backend/obj/Debug/net7.0/Brewtiful.AssemblyInfo.cs
+++ backend/obj/Debug/net7.0/Brewtiful.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Brewtiful")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58eeb338391f546f6b1c34d25bfe52a41a3287ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bcf5c40466424012822b2ca66cca228e7d9af9d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Brewtiful")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Brewtiful")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
